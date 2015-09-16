cc79ad1ad8f7b6a53b9813442280db62d847476c
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-09-16 13:41:23 -0600

    Add single get request

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,13 +3,30 @@ var express = require('express'),
   mongoose = require('mongoose-q')(require('mongoose'), {spread:true}),
   Exercise = require('../models/exercise.js');
 
+// HTTP get all request
 router.get('/exercises', function(req, res, next) {
   Exercise.findQ()
-    .then(function (result) { res.json(result) })
-    .catch(function (err) {res.send(err) })
+    .then(function(result) {
+      res.json(result);
+    })
+    .catch(function(err) {
+      res.send(err);
+    })
     .done();
 });
-
+// HTTP get single request
+router.get('/exercises/:id', function(req, res, next) {
+  var query = {'_id': req.params.id};
+  Exercise.findByIdQ(query)
+    .then(function(result) {
+      res.json(result);
+    })
+    .catch(function(err) {
+      res.send(err);
+    })
+    .done();
+});
+// HTTP post one request
 router.post('/exercises', function(req, res, next) {
   new Exercise(req.body)
     .saveQ(function(err, data) {
@@ -19,6 +36,17 @@ router.post('/exercises', function(req, res, next) {
       res.json(data);
     }
   });
+});
+// HTTP put single request
+router.put('/exercises/:id', function(req, res, next) {
+  var query = {'_id': req.params.id}, options = {new: true};
+  Exercise.findOneAndUpdateQ(query, options)
+    .then(function(result) {
+      res.json(result);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
 });
 
 module.exports = router;
